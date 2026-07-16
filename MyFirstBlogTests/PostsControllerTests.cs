e6a103113585bcef6180113aedd49bce48615846
fishpin <120992896+fishpin@users.noreply.github.com>
2026-07-15 21:37:40 -0600

    test: add TDD coverage for POST + add .vs/ to gitignore

---
+++ MyFirstBlogTests/PostsControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyFirstBlogTests
+{
+    public class PostsControllerTests
+    {
+        private readonly HttpClient _client = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5000")
+        };
+
+        private Task<HttpResponseMessage> PostAsync(object body)
+        {
+            var json = JsonSerializer.Serialize(body);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            return _client.PostAsync("/posts", content);
+        }
+
+        [Test]
+        public async Task CreatePost_WithValidData_Returns201WithPost()
+        {
+            var response = await PostAsync(new { title = "some title", description = "some content" });
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+
+            var body = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+            var post = body.RootElement.GetProperty("post");
+
+            Assert.That(post.GetProperty("title").GetString(), Is.EqualTo("some title"));
+            Assert.That(post.GetProperty("description").GetString(), Is.EqualTo("some content"));
+        }
+
+        [Test]
+        public async Task CreatePost_WithBlankTitle_Returns400WithErrors()
+        {
+            var response = await PostAsync(new { title = "", description = "some content" });
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+
+            var body = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+            var errors = body.RootElement.GetProperty("errors");
+
+            Assert.That(errors[0].GetString(), Is.EqualTo("Title cannot be blank"));
+        }
+    }
+}
